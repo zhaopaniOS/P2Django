143bc4fbc469f12facf915709d8ee7acd4dcd60e
zhaopaniOS <zhaopan@126.com>
2018-05-07 08:58:16 +0800

    add Post content to homepage view......

--- myweb/mainsite/views.py
+++ myweb/mainsite/views.py
@@ -7,5 +7,6 @@ def homepage(request):
     posts = Post.objects.all()
     post_lists = list()
     for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+        post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
+        post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
     return HttpResponse(post_lists)
