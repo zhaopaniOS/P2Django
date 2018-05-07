ca91a933613ba9f6d3f05591e29cd46e133324f9
zhaopaniOS <zhaopan@126.com>
2018-05-07 09:10:39 +0800

    using templates insteadof views response......

--- myweb/mainsite/views.py
+++ myweb/mainsite/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
 
 # Create your views here.
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
-        post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
