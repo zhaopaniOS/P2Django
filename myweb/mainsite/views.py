856c414cab25fac02829e44326d828b650b5b1dd
zhaopaniOS <zhaopan@126.com>
2018-05-07 09:25:42 +0800

    add post.html in templates......

--- myweb/mainsite/views.py
+++ myweb/mainsite/views.py
@@ -10,3 +10,13 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+def showpost(request, id):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(id=id)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return HttpResponse('/')
