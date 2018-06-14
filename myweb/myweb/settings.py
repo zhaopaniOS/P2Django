d7ae6625de11a3c8d86527b76bd17ac07170bf0e
zhaopaniOS <zhaopan@126.com>
2018-06-14 10:49:12 +0800

    将设置调整为产品发布模式。。。。。。

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ee4%@%7ele%#jfgdg4xf=x+7*5&+45lp-)hb@qlca#z17ysufx'
+#SECRET_KEY = 'ee4%@%7ele%#jfgdg4xf=x+7*5&+45lp-)hb@qlca#z17ysufx'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['47.96.88.244', 'qdota.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +125,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = '/var/www/staticfiles'
